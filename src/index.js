bbee706d2bfeab05a0fb85c18ce2beb35d40dd7a
Samuel Maiko <sammymaiko721@gmail.com>
2023-06-26 14:55:08 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
   
+    const form=document.querySelector('form');
+    form.addEventListener('submit',(e)=>{
+e.preventDefault()
+const userInput=document.querySelector('#searchByID').value
+fetch(`http://localhost:3000/movies/${userInput}`)
+.then(response=>response.json())
+.then(data =>{
+    const title=document.querySelector('#movieDetails h4:nth-of-type(1)')
+    const summary=document.querySelector('#movieDetails p:nth-of-type(1)')
+    title.innerHTML=data.title
+    summary.innerHTML=data.summary
+})
+form.reset()
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
